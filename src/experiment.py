5f9c2c2e6c3e9cb01caf9a506263fdf541d48f11
Mohammad Joshaghani <71117558+mohammadjoshaghani@users.noreply.github.com>
2023-07-25 15:57:13 +0430

    add experiment manager and runner

---
+++ src/experiment.py
@@ -0,0 +1,61 @@
+import time, os
+from runner import Runner
+import wandb
+
+
+class Optimizer:
+    def __init__(self, FH, ExpId):
+        self.FH = FH
+        self.ExpId = ExpId
+    
+    def objective(self):      
+        
+        wandb.init()
+        epochs, weightDecay, lr = wandb.config.epochs, wandb.config.weightDecay, round(wandb.config.learning_rate,ndigits=4)
+        modes_loss_rmse = {}
+
+        # run model
+        # self.runner = Runner(mode,ExpId,FH,epochs,lr,weightDecay)
+        for mode in ['train', 'valid', 'test']:
+            self.runner = Runner(mode,self.ExpId,self.FH,epochs,lr,weightDecay)
+            print(f"\n### start {mode} phase for wd_{weightDecay}_lr_{lr}_epochs_{epochs}:\n")
+            self.runner.run()
+            
+            if mode!="train":
+                # save loss for each mode
+                modes_loss_rmse[mode] = self.runner.rmse           
+            if mode=="valid":
+                mlp_mse = self.runner.mfeatur_model.runner_mlp.loss
+                tcn_mse = self.runner.mfeatur_model.runner_tcn.loss
+                lstm_att_mse = self.runner.mfeatur_model.runner_lstm_att.loss
+        # validation and test loss:
+        valid_loss_rmse, test_loss_rmse = modes_loss_rmse['valid'], modes_loss_rmse['test']
+        # Log model performance metrics to W&B
+        wandb.log({"valid_loss_rmse": valid_loss_rmse, "test_loss_rmse": test_loss_rmse, "mlp_mse":mlp_mse, "tcn_mse":tcn_mse,"lstm_att_mse":lstm_att_mse})
+
+os.environ['WANDB_SILENT']="true"
+os.environ['WANDB_MODE']="offline"
+
+print("\nstart:\n")
+s_time = time.time()
+
+FH = 7
+ExpId = '01'
+
+optimizer = Optimizer(FH, ExpId)
+sweep_configs = {
+    "method": "grid",
+    "metric": {"name": "valid_loss_rmse", "goal": "minimize"},
+    "parameters": {
+        "epochs": {"values": [1]},
+        "weightDecay": {"values": [0.001, 0.005, 0.009]},
+        # "learning_rate": {"distribution": "uniform", "min": 0.0100, "max": 0.0400},
+        "learning_rate":{"values": [0.01, 0.02, 0.03, 0.05, 0.06]},
+    },
+}
+
+sweep_id = wandb.sweep(sweep_configs, project=f"FH{FH}_{ExpId}")
+wandb.agent(sweep_id=sweep_id, function=optimizer.objective, count=1)
+
+print(f"\n total time: {time.time()-s_time :.2f} seconds.")
+print("\n finish.")
---
+++ src/runner.py
@@ -0,0 +1,170 @@
+import torch
+import pickle
+import numpy as np
+import os
+from sklearn.ensemble import RandomForestClassifier
+from dataset.data import Dataset, mode_indx
+from m_features import Meta_Features
+from metats.pipeline import MetaLearning
+
+class Runner():
+    def __init__(self, mode,ExpId,FH,epochs,lr,weightDecay):
+        self.ExpId = ExpId #
+        self.mode = mode
+        self._init_mkdir() #
+        self.path_base_models = "base_forecasters/_all_npy/"#
+        self.path_true_d = "src/dataset/_true_npy/"#
+        self.FH = FH#
+        self._check_gpu()#
+        _ = self.load_data()
+        self.batchsize = 2#
+        self.mfeatur_model = Meta_Features(self.device,self.x_lstm_att.size(2), self.x_tcn.size(2), self.x_tcn.size(1),epochs,lr,weightDecay,self.batchsize)#
+        _ = self.load_clf()#
+        _ = self._check_mode()
+        
+    def run(self):
+        meta_features = self.get_mfeatures()
+        pipeline = MetaLearning(method='averaging', loss='mse')
+        pipeline.add_metalearner(self.clf)# done
+        predictions = self.load_predictions()[:20,:,:]   # done
+        if self.train_clf:
+            labels = pipeline.generate_labels(self.x_true, predictions)
+            labels[0:8]=[i for i in range(8)]
+            assert len(set(labels))==8, f"the # of labels are not 8!"
+            pipeline.meta_learner.fit(X=meta_features, y=labels)
+        else:
+            # test
+            weights = pipeline.predict_generate_weights(meta_features)
+            self.final_forecast = pipeline.averaging_predictions(weights, predictions)
+        self.save_results()
+    
+    def save_results(self):
+        if self.mode == "train":
+            self.save_models()
+            self.save_clf()
+        else:
+            self.save_forecasts()
+            self.call_rmse()    
+    
+    def call_rmse(self):
+            mse = np.square(np.subtract(self.x_true,self.final_forecast)).mean() 
+            self.rmse = np.sqrt(mse)
+
+    def save_forecasts(self):
+        np.savetxt(self.path +f'FH_{self.FH}'+f'_{self.mode}'+'.csv', self.final_forecast, delimiter=",")
+        # print(self.final_forecast.shape)
+
+    def get_mfeatures(self):
+        if self.models_gradient:
+            self.mfeatur_model.feed_inputs(self.x_tcn, self.x_lstm_att, gradient=True)
+            self.mfeatur_model.feed_inputs(self.x_tcn, self.x_lstm_att, gradient=False)
+            meta_features = self.mfeatur_model.runner_mlp.latent
+        else:
+            # load saved parameters
+            _= self.load_models()
+            self.mfeatur_model.feed_inputs(self.x_tcn, self.x_lstm_att, gradient=False)
+            meta_features = self.mfeatur_model.runner_mlp.latent
+        return meta_features.reshape(meta_features.size(0),-1).detach().numpy()
+    
+    def _check_mode(self):
+        # it doesn't need to train the deep auto-encoders for all self.FH's  
+        self.models_gradient = (self.mode =="train" and self.FH==7)
+        # only clf is needed to be trained for all self.FH's
+        self.train_clf = (self.mode=="train")
+
+    def load_data(self,initial=False):
+        if initial:
+            data = Dataset(self.mode)
+            # true
+            self.x_true = data.x[:,48:,0]
+            # main
+            self.x_lstm_att = data.x_norm[:,:48,0:1]
+            # factors
+            self.x_tcn = data.x_norm[:,:48,1:5]
+            # save:
+            np.save(self.path_true_d+'x_true.npy',self.x_true)
+            np.save(self.path_true_d+'x_lstm_att.npy',self.x_lstm_att.numpy())
+            np.save(self.path_true_d+f'x_tcn.npy',self.x_tcn.numpy())
+
+        else:    
+            self.x_true = np.load(self.path_true_d+'x_true.npy')
+            self.x_lstm_att = torch.from_numpy(np.load(self.path_true_d+'x_lstm_att.npy'))
+            self.x_tcn = torch.from_numpy(np.load(self.path_true_d+f'x_tcn.npy'))
+        
+        # adjust x_true based on self.FH
+        self.x_true = self.x_true[:,:self.FH]
+        # to gpu if available
+        self.x_lstm_att, self.x_tcn = self.x_lstm_att.to(self.device), self.x_tcn.to(self.device)    
+    
+    def _check_gpu(self):
+        # run on GPU if available:
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    def load_predictions(self, initial=False):
+        # create (# time series, # models, horizon) predictions
+        #  for each mode = ["train","valid","test"]
+        if initial:
+            predictions = self.read_predictions(ifsave=True)
+        else:
+            predictions = np.load(self.path_base_models+f'base_{self.mode}.npy')    
+        return predictions[:,:,:self.FH]
+
+    def read_predictions(self,ifsave=True):
+        (star,end) = mode_indx(self.mode)
+        models = [m for m in os.listdir('base_forecasters') if m!='_all_npy']
+        slot_i = []
+        for i in range(start,end):    
+            base_is = []
+            for model in models:
+                base_i = np.genfromtxt(f'base_forecasters\{model}\{model}_{i}.csv', delimiter=',')[1:,1:]
+                base_is.append(np.expand_dims(base_i,axis=1))
+            is_model = np.concatenate(base_is, axis=1)   
+            slot_i.append(is_model)
+        slots = np.concatenate(slot_i, axis=0)
+        if ifsave:
+            np.save(self.path_base_models+f'base_{self.mode}.npy',slots)
+        return slots    
+
+    def save_models(self):
+        torch.save(self.mfeatur_model.runner_tcn.model.state_dict(), self.path +'model_tcn.pt')
+        torch.save(self.mfeatur_model.runner_lstm_att.model.state_dict(), self.path +'model_lstm_att.pt')
+        torch.save(self.mfeatur_model.runner_mlp.model.state_dict(), self.path +'model_mlp.pt')
+    
+    def load_models(self):
+        self.mfeatur_model.runner_tcn.model.load_state_dict(torch.load(self.path +'model_tcn.pt'))
+        self.mfeatur_model.runner_lstm_att.model.load_state_dict(torch.load(self.path +'model_lstm_att.pt'))
+        self.mfeatur_model.runner_mlp.model.load_state_dict(torch.load(self.path +'model_mlp.pt'))
+    
+    def _init_mkdir(self):
+        __root_path = os.getcwd()
+        path = os.path.join(__root_path,'results/')
+        path += 'ExpId_'+str(self.ExpId) + '/'
+        path += 'tcn_4_1'+ '_' 
+        path += 'lstm_1024_256'+ '_' 
+        path += 'mlp_128_8'+ '_' 
+        # path += 'WD_'+str(self.weightDecay)+'_'
+        # path += 'lr_'+str(self.lr) + '_'
+        # path += 'epochs_'+str(self.epochs)+ '_'
+        path += '/'
+        if not os.path.exists(path):
+            os.makedirs(path)
+        self.path = path
+        # print(self.path)
+        pass 
+    
+    def save_clf(self):
+            pickle.dump(self.clf, open(self.path+f'clf_FH{self.FH}.pickle', "wb"))
+    
+    def load_clf(self):
+        if self.mode =="train":
+            self.clf = RandomForestClassifier()
+        else:
+            self.clf = pickle.load(open(self.path+f'clf_FH{self.FH}.pickle', "rb"))
+
+if __name__ == "__main__":
+    (mode,ExpId,FH,epochs,lr,weightDecay)=("train","01",1,1,0.01,0.009)
+    arg = (mode,ExpId,FH,epochs,lr,weightDecay)
+    runner = Runner(*arg)
+    preds = runner.run()
+    # preds = runner.save_clf()
+    # print(preds.shape)
